2cc93245b59696348cf887e04188d91328eb6980
Bianca Regulski <biancaregulski@yahoo.com>
2020-03-03 11:12:23 -0500

    Fix database funcions

--- RoommateMatcher/ResidentDatabase.cpp
+++ RoommateMatcher/ResidentDatabase.cpp
@@ -1,17 +1,28 @@
 #include <sstream> 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+#include <cstdlib>
+#include <regex>
 #include <sqlite3.h>
+
+using Record = std::vector<std::string>;
+using Records = std::vector<Record>;
 
 static int createDB(const char* dir);
 static int createTable(const char* dir);
-static int insertData(const char* dir, std::string id, std::string nextId, std::string roommateId, std::string firstName,
-	std::string lastName, int temperature, int cleanliness, int visitors, int smoker, int showers);
-static int selectData(const char* dir, std::string selection);
+static int insertData(const char* dir, std::string id, std::string roommateId, std::string firstName,
+					  std::string lastName, int temperature, int cleanliness, int visitors, int smoker, int showerTime);
+static bool isEmpty(const char* dir);
+static Records selectData(const char* dir, const std::string& id, const std::string& attribute);
+//int select_callback(void* p_data, int num_fields, char** p_fields, char** p_col_names);
 static int updateData(const char* dir, std::string id, std::string attribute, std::string value);
 static int deleteData(const char* dir, std::string id);
 static int callback(void* notUsed, int argc, char** argv, char** azColName);
 static std::string intToString(int num);
 static int stringToInt(std::string string);
+
 
 static int createDB(const char* dir) {
 	sqlite3* DB;
@@ -28,16 +39,15 @@ static int createTable(const char* dir) {
 	sqlite3* DB;
 
 	std::string sql = "CREATE TABLE IF NOT EXISTS RESIDENTS("
-		"ID				TEXT PRIMARY KEY, "
-		"NEXTID			TEXT NULL, "
-		"ROOMMATEID		TEXT NULL, "
-		"FIRSTNAME		TEXT NOT NULL, "
-		"LASTNAME		TEXT NOT NULL, "
-		"TEMPERATURE	INT NOT NULL, "
-		"CLEANLINESS	INT NOT NULL, "
-		"VISITORS		INT NOT NULL, "
-		"SHOWERS		INT NOT NULL, "
-		"SMOKER			INT NOT NULL);";
+		"ID				VARCHAR(10) PRIMARY KEY, "
+		"Roommate_ID	VARCHAR(10) NOT NULL, "
+		"First_Name		VARCHAR(30) NOT NULL, "
+		"Last_Name		VARCHAR(30) NOT NULL, "
+		"Temperature	INT NOT NULL, "
+		"Cleanliness	INT NOT NULL, "
+		"Visitors		INT NOT NULL, "
+		"Shower_Time	INT NOT NULL, "
+		"Smoker			INT NOT NULL);";
 
 	try {
 		int exit = 0;
@@ -59,32 +69,37 @@ static int createTable(const char* dir) {
 	return 0;
 }
 
-static int insertData(const char* dir, std::string id, std::string nextId, std::string roommateId, std::string firstName, 
-					  std::string lastName, int temperature,  int cleanliness, int visitors, int smoker, int showers) {
+static int insertData(const char* dir, std::string id, std::string roommateId, std::string firstName, std::string lastName,
+					  int temperature, int cleanliness, int visitors, int smoker, int showerTime) {
 	sqlite3* DB;
-	char* errorMsg;
+	char* errorMsg = 0;
 	int exit = sqlite3_open(dir, &DB);
 
-	std::string sql = "INSERT INTO USER (ID, NEXTID, ROOMMATEID, FIRSTNAME, LASTNAME, TEMPERATURE, CLEANLINESS, VISITORS, SHOWERS, SMOKER) VALUES ('"
-			+ id + "', '" + nextId + "', " + roommateId + "', " + firstName + "', " + lastName + ", " + intToString(temperature) + ", " 
-			+ intToString(cleanliness) + ", " + intToString(visitors) + ", " + intToString(smoker) + ", " + intToString(showers) + ");";
+	std::string sql = "INSERT INTO RESIDENTS (ID, Roommate_ID, First_Name, Last_Name, Temperature, Cleanliness, Visitors, Shower_Time, Smoker) VALUES ('"
+		+ id + "', '" + roommateId + "', '" + firstName + "', '" + lastName + "', " + intToString(temperature - 1) + ", "
+		+ intToString(cleanliness - 1) + ", " + intToString(visitors - 1) + ", " + intToString(showerTime - 1) + ", " + intToString(smoker - 1) + ");";
 
 	exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &errorMsg);
 	if (exit != SQLITE_OK) {
-		std::cerr << "Error: could not insert\n";
+		std::cerr << "Error: " <<  errorMsg << '\n';
 		sqlite3_free(errorMsg);
 	}
+	else {
+		std::cout << "\n" << firstName << ' ' << lastName << " added to the list of residents."
+				  << "\nResident ID: " << id << "\n\n";
+	}
+
 	return 0;
 }
 
 static int updateData(const char* dir, std::string id, std::string attribute, std::string value) {
-	sqlite3 * DB;
-	char * errorMsg;
+	sqlite3* DB;
+	char* errorMsg;
 	int exit = sqlite3_open(dir, &DB);
 	char punct = ' ';
 	// if attribute is a string, then we need to add apostrophes to the query
-	if (attribute.compare("ID") == 0 || attribute.compare("NEXTID") == 0 || attribute.compare("ROOMMATEID") == 0
-		|| attribute.compare("FIRSTNAME") == 0 || attribute.compare("LASTNAME") == 0) {
+	if (attribute.compare("ID") == 0 || attribute.compare("Roommate_ID") == 0
+		|| attribute.compare("First_Name") == 0 || attribute.compare("Last_Name") == 0) {
 		punct = '\'';
 	}
 	std::string sql = "UPDATE RESIDENTS SET " + attribute + " = " + punct + value + punct + " WHERE ID = '" + id + "';";
@@ -93,47 +108,97 @@ static int updateData(const char* dir, std::string id, std::string attribute, st
 		std::cerr << "Error: could not update\n";
 		sqlite3_free(errorMsg);
 	}
+	else {
+		std::cout << attribute << " attribute successfully changed.\n";
+	}
 	return 0;
 }
 
-static int selectData(const char* dir, std::string selection) {
-	sqlite3 * DB;
+static bool isEmpty(const char* dir) {
+	sqlite3* DB;
 	int exit = sqlite3_open(dir, &DB);
-	std::string sql = "SELECT " + selection + " FROM RESIDENTS;";
-	sqlite3_exec(DB, sql.c_str(), callback, NULL, NULL);
+	const char* sql = "SELECT COUNT(*) FROM RESIDENTS";
+	struct sqlite3_stmt* selectstmt;
+	int result = sqlite3_prepare_v2(DB, sql, -1, &selectstmt, NULL);
+	if (result == SQLITE_OK)
+	{
+		if (sqlite3_step(selectstmt) == SQLITE_ROW)
+		{
+			return false;		// row found
+		}
+		else
+		{
+			return true;
+		}
+	}
+	sqlite3_finalize(selectstmt);
+}
 
+// id and attribute are optional paramaters. to get count, only call with dir parameter
+static Records selectData(const char* dir, const std::string &id, const std::string &attribute) {
+	Records records;  
+	sqlite3* DB;
+	char* errorMsg;
+	std::string sql;
+	int exit = sqlite3_open(dir, &DB);
+	sql = "SELECT " + attribute + " FROM RESIDENTS WHERE ID = '" + id + "';";
+	exit = sqlite3_exec(DB, sql.c_str(),callback, &records, &errorMsg);
+
+	if (exit != SQLITE_OK) {
+		std::cerr << "Error: " << errorMsg << '\n';
+		sqlite3_free(errorMsg);
+	}
+
+	return records;
+}
+
+int callback(void* p_data, int num_fields, char** p_fields, char** p_col_names) {
+	Records* records = static_cast<Records*>(p_data);
+	try {
+		records->emplace_back(p_fields, p_fields + num_fields);
+	}
+	catch (...) {
+		return 1;
+	}
 	return 0;
 }
+
 
 static int deleteData(const char* dir, std::string id) {
-	sqlite3 * DB;
+	sqlite3* DB;
 	int exit = sqlite3_open(dir, &DB);
 	std::string sql = "DELETE FROM RESIDENTS WHERE ID = '" + id + "';";
-	sqlite3_exec(DB, sql.c_str(), callback, NULL, NULL);
+	exit = sqlite3_exec(DB, sql.c_str(), callback, NULL, NULL);
+
+	if (sqlite3_changes(DB) > 0) {
+		std::cout << id << " has been deleted.";
+	}
+	else {
+		std::cout << id << " not found.";
+	}
 
 	return 0;
 }
 
 // retrieves database contents
-static int callback(void* notUsed, int argc, char** argv, char** azColName) {
+/*static int callback(void* notUsed, int argc, char** argv, char** azColName) {
 	for (int i = 0; i < argc; i++) {
 		std::cout << azColName[i] << ": " << argv[i] << std::endl;
 	}
 	std::cout << std::endl;
 
 	return 0;
-}
+}*/
 
 static std::string intToString(int num) {
 	std::ostringstream ss;
-	ss << num;                                   
+	ss << num;
 	return ss.str();
 }
 
 static int stringToInt(std::string string) {
-	std::cout << "test";
 	int num = 0;
 	std::istringstream ss(string);
-	ss >> num;                                 
+	ss >> num;
 	return num;
 }
--- RoommateMatcher/RoommateMatcher.cpp
+++ RoommateMatcher/RoommateMatcher.cpp
@@ -1,126 +1,206 @@
-// TODO: make database where you can query by name, preferences, etc.
 // TODO: algorithm
-#include "ResidentList.cpp"
+#include "ResidentDatabase.cpp"
 #include <boost/algorithm/string/trim.hpp>
+
+
+const char* levelNames[3] = { "Low", "Moderate", "High" };
+const char* timeNames[3] = { "Mornings", "Afternoons", "No preference" };
+const char* yesNo[2] = { "Yes", "No" };
 
 void showMenu();
 std::string nameErrorChecking();
 int intErrorChecking(int rangeMax);
+void displayResidentInfo(Record row);
+void displayEditMenu(Record row);
+std::string getFirstName();
+std::string getLastName();
+int getTemperature();
+int getCleanliness();
+int getVisitors();
+int getShowerTime();
+int getSmoker();
 
 int main() {
 
-    const char* dir = "STUDENTS.db";      // TODO: change?
+    const char* dir = "RESIDENTS.db";
     sqlite3* DB;
     createDB(dir);
     createTable(dir);
 
-    ResidentList residents;
-    ResidentNode* selectedResident;
     bool exit = false;
     int selection;
-    std::string firstName, lastName, id;
-    int temperature, cleanliness, visitors, showers;
+    Records records;
+    int idNum;
+    std::string idNumString;
+    std::string firstName, lastName, id, roommateId;
+    int temperature, cleanliness, visitors, showerTime;
     int smoker;
-
+    Records zrecords;
     showMenu();
 
     do {
         std::cout << "\nSelection: ";
         std::cin >> selection;
         switch (selection) {
-            case 1:
-                std::cout << "\nFirst Name: ";
-                std::cin.ignore();                              // ignores newline before std::getline
-                firstName = nameErrorChecking();
-                std::cout << "Last Name: ";
-                lastName = nameErrorChecking();
+        case 1:
+            firstName = getFirstName();
+            lastName = getLastName();
+            temperature = getTemperature();
+            cleanliness = getCleanliness();
+            visitors = getVisitors();
+            showerTime = getShowerTime();
+            smoker = getSmoker();
 
-                std::cout << "\nWhat temperature should your room be?\n"
-                          << "1. Cold\n2. Average\n3. Warm\n> ";
-                temperature = intErrorChecking(3);                          // checks that 1 =< input >= 3
+            // build id by appending strings
+            id = "";
+            id += std::tolower(firstName[0]);                // id starts with first and last initial
+            id += std::tolower(lastName[0]);
+            idNum = 0;
+            do {
+                idNumString = intToString(++idNum);
+            } while (!selectData(dir, id + idNumString, "*").empty());
 
-                std::cout << "\nHow clean is your room?\n"
-                          << "1. Tidy\n2. Average\n3. Messy\n> ";
-                cleanliness = intErrorChecking(3);
+            id += idNumString;                               // then append id number to initials
 
-                std::cout << "\nHow often may visitors be over?\n"
-                    << "1. Infrequently\n2. Average\n3. Frequently\n> ";
-                visitors = intErrorChecking(3);
+            std::cout << id << '\n';
 
-                std::cout << "\nWhen do you like to take showers?\n"
-                          << "1. Mornings\n2. Afternoons\n3. No preference\n> ";
-                showers = intErrorChecking(3);
+            roommateId = "None";
+            insertData(dir, id, roommateId, firstName, lastName, temperature, cleanliness, visitors, smoker, showerTime);
 
-                std::cout << "\nAre you a smoker?\n"
-                    << "1. Yes\n2. No\n> ";
-                smoker = intErrorChecking(2);
-
-                id = residents.addResident(firstName, lastName, temperature,
-                    cleanliness, visitors, showers, smoker);
-
-                std::cout << "\n" << firstName << ' ' << lastName << " added to the list of residents."
-                          << "\nResident ID: " << id << "\n\n";
-                showMenu();
-                break;
-            case 2:
-                if (residents.isEmpty()) {
-                    std::cout << "List of residents is empty.\n";
+            showMenu();
+            break;
+        case 2:
+            // TODO: check if list is empty
+            std::cout << "\nResident ID: ";
+            std::cin >> id;
+            records = selectData(dir, id, "*");
+            if (records.empty()) {
+                std::cout << "Resident not found.\n\n";
+            }
+            else {
+                for (Record & row : records) {
+                    displayResidentInfo(row);
                 }
-                else {
-                    std::cout << "\nResident ID: ";
-                    std::cin >> id;
-                    selectedResident = residents.searchNode(id);
-                    if (selectedResident == NULL) {
-                        std::cout << "Resident not found.\n\n";
+            }
+            showMenu();
+            break;
+        case 3:
+            // TODO: check if list is empty
+            std::cout << "\nResident ID: ";
+            std::cin >> id;
+            records = selectData(dir, id, "*");
+            if (records.empty()) {
+                std::cout << "Resident not found.\n\n";
+            }
+            else {
+                std::cout << "\nWhich attribute would you like to edit?\n";
+                for (auto& row : records) {
+                    displayEditMenu(row);
+                }
+                int editSelection;
+                do {
+                    std::cout << "\n> ";
+                    editSelection = intErrorChecking(8);                // input number to find out which attribute to change
+                    switch (editSelection) {
+                    case 1:
+                        firstName = getFirstName();
+                        updateData(dir, id, "First_Name", firstName);
+                        break;
+                    case 2:
+                        lastName = getLastName();
+                        updateData(dir, id, "Last_Name", lastName);
+                        break;
+                    case 3:
+                        temperature = getTemperature();
+                        updateData(dir, id, "Temperature", intToString(temperature));
+                        break;
+                    case 4:
+                        cleanliness = getCleanliness();
+                        updateData(dir, id, "Cleanliness", intToString(cleanliness));
+                        break;
+                    case 5:
+                        visitors = getVisitors();
+                        updateData(dir, id, "Visitors", intToString(visitors));
+                        break;
+                    case 6:
+                        showerTime = getShowerTime();
+                        updateData(dir, id, "Shower_Time", intToString(showerTime));
+                        break;
+                    case 7:
+                        smoker = getSmoker();
+                        updateData(dir, id, "Smoker", intToString(smoker));
+                    case 8: 
+                        break;
+                    default:
+                        std::cout << "Invalid selection. Try again.\n";
                     }
-                    else {
-                        residents.displayResidentInfo(selectedResident);
-                        std::cout << "\n\n";
-                    }
-                    showMenu();
-                }
-                break;
-            case 3:
-                if (residents.isEmpty()) {
-                    std::cout << "List of residents is empty.\n";
-                }
-                else {
-                    std::cout << "\nResident ID: ";
-                    std::cin >> id;
-                    selectedResident = residents.searchNode(id);
-                    if (selectedResident == NULL) {
-                        std::cout << "Resident not found.\n\n";
-                    }
-                    else {
-                        std::cout << "\nWhich attribute would you like to edit?\n";
-                        residents.displayEditMenu(selectedResident);
-                        std::cout << "\n> ";
-                    }
-                    showMenu();
-                }
-                break;
-            case 4:
-                std::cout << "\nResident ID: ";
-                std::cin >> id;
-                if (residents.deleteNode(id)) {
-                    std::cout << "\nResident deleted from list.\n\n";
-                    // TODO: if resident has roommate, set roommate's roommate to null
-                }
-                else {
-                    std::cout << "Resident not found.\n";
-                }
-                showMenu();
-                break;
-            case 5:
-                // pair algorithm
-                break;
-            case 6:
-                exit = true;
-                break;
-            default:
-                std::cout << "Invalid selection. Try again.\n";
+                } while (editSelection < 0 || editSelection > 8);
+            }
+            std::cout << '\n';
+            showMenu();
+            break;
+        case 4:
+            std::cout << "\nResident ID: ";
+            std::cin >> id;
+            deleteData(dir, id);
+            // TODO: if resident has roommate, set roommate's roommate to null
+            std::cout << "\n\n";
+            showMenu();
+            break;
+        case 5:
+            // pair algorithm
+            break;
+        case 6:
+            exit = true;
+            break;
+        default:
+            std::cout << "Invalid selection. Try again.\n";
         }
     } while (exit == false);
+}
+
+std::string getFirstName() {
+    std::cout << "\nFirst Name: ";
+    std::cin.ignore();                              // ignores newline before std::getline
+    return nameErrorChecking();
+}
+
+std::string getLastName() {
+    std::cout << "Last Name: ";
+    return nameErrorChecking();
+}
+
+int getTemperature() {
+    std::cout << "\nWhat temperature should your room be?\n"
+        << "1. Cold\n2. Average\n3. Warm\n> ";
+    return intErrorChecking(3);                          // checks that 1 =< input >= 3
+}
+
+int getCleanliness() {
+    std::cout << "\nHow clean is your room?\n"
+        << "1. Tidy\n2. Average\n3. Messy\n> ";
+    return intErrorChecking(3);
+}
+
+int getVisitors() {
+
+
+    std::cout << "\nHow often may visitors be over?\n"
+        << "1. Infrequently\n2. Average\n3. Frequently\n> ";
+    return intErrorChecking(3);
+}
+
+int getShowerTime() {
+
+    std::cout << "\nWhen do you like to take showers?\n"
+        << "1. Mornings\n2. Afternoons\n3. No preference\n> ";
+    return intErrorChecking(3);
+}
+
+int getSmoker() {
+    std::cout << "\nAre you a smoker?\n"
+        << "1. Yes\n2. No\n> ";
+    return intErrorChecking(2);
 }
 
 std::string nameErrorChecking() {
@@ -176,3 +256,32 @@ void showMenu() {
               << "* * * * * * * * * * * * *" << '\n';
 }
 
+void displayResidentInfo(Record row) {
+
+    std::cout << "\nName: " << row.at(2) << ' ' << row.at(3);           // output first and last name
+    std::cout << "\nRoommate: ";
+    //if (row.at(1).empty()) {
+        std::cout << "Unassigned";
+   /* }
+    else {
+        // TODO: retrieve assigned roommate
+    }*/
+    std::cout << "\nPreferences"
+        << "\n-----------"
+        << "\nTemperature: " << levelNames[stringToInt(row.at(4))]
+        << "\nCleanliness: " << levelNames[stringToInt(row.at(5))]
+        << "\nVisitors: " << levelNames[stringToInt(row.at(6))]
+        << "\nShower Time: " << timeNames[stringToInt(row.at(7))]
+        << "\nSmoker: " << yesNo[stringToInt(row.at(8))] << "\n\n";
+}
+
+void displayEditMenu(Record row) {
+    std::cout << "\n1. First name: " << row.at(2)
+        << "\n2. Last name: " << row.at(3)
+        << "\n3. Temperature: " << levelNames[stringToInt(row.at(4))]
+        << "\n4. Cleanliness: " << levelNames[stringToInt(row.at(5))]
+        << "\n5. Visitors: " << levelNames[stringToInt(row.at(6))]
+        << "\n6. Shower Time: " << timeNames[stringToInt(row.at(7))]
+        << "\n7. Smoker: " << yesNo[stringToInt(row.at(8))]
+        << "\n8. Cancel";
+}
